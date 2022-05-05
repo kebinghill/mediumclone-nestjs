82c716f8451c73e458240639116cc6ed7b92067f
Kevin Gil <93kevingil@gmail.com>
2022-05-05 17:48:41 -0300

    createUser DTO

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '@app/user/dto/createUser.dto';
 
 @Injectable()
 export class UserService {
-  async createUser() {
-    return 'createUser from service';
+  async createUser(createDto: CreateUserDto) {
+    return createDto;
   }
 }
