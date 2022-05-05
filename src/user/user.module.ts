09838416c0376cc1b05beb59595fb616beeb2a97
Kevin Gil <93kevingil@gmail.com>
2022-05-05 19:56:46 -0300

    creating user entity and hash pw with bcrypt

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from '@app/user/user.controller';
 import { UserService } from '@app/user/user.service';
+import { UserEntity } from '@app/user/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
   providers: [UserService],
 })
